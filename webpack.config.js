51b0b69dbde7c8f0f29ab540e34f49495088e7aa
Pavlo Glazkov <p.glazkov@gmail.com>
2017-04-23 14:57:11 +0200

    Better TSLint setup (new rules + other infrastructure changes)

--- webpack.config.js
+++ webpack.config.js
@@ -239,14 +239,8 @@ module.exports = function (args = {}) {
               sourceMap: false
             },
             tslint: {
-              // tslint errors are displayed by default as warnings 
-              // set emitErrors to true to display them as errors 
-              emitErrors: true,
-
-              // tslint does not interrupt the compilation by default 
-              // if you want any file with tslint errors to fail 
-              // set failOnHint to true 
-              failOnHint: false
+              typeCheck: true,    
+              tsConfigFile: helpers.root(srcPath, 'tsconfig.app.json'),
             },
             context: ''
           }
