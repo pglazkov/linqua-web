bebf72ebfb89ad43856ba1f21f761f6726d648c9
Pavlo Glazkov <p.glazkov@gmail.com>
2017-08-20 10:11:01 +0200

    Add enter/leave animations for the entry card

--- src/client/app/+home/home-view/home-view.component.ts
+++ src/client/app/+home/home-view/home-view.component.ts
@@ -2,11 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
 import { EntryEditorDialogComponent } from '../entry-editor-dialog/entry-editor-dialog.component';
 import { Entry } from 'shared';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
-  styleUrls: ['./home-view.component.scss']
+  styleUrls: ['./home-view.component.scss'],
+  animations: [
+    trigger('entryCardEnterLeave', [
+      state('in', style({ opacity: 1, height: '*' })),
+      transition('void => new', [
+        animate('0.4s', keyframes([
+          style({ opacity: 0, height: 0, offset: 0 }),
+          style({ opacity: 0, height: '*', offset: 0.3 }),
+          style({ opacity: 1, height: '*', offset: 1.0 })
+        ]))
+      ]),
+      transition('* => void', [
+        animate('0.2s ease-in', style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class HomeViewComponent implements OnInit {
 
@@ -38,7 +54,10 @@ export class HomeViewComponent implements OnInit {
   addNewEntry() {
     this.dialog.open(EntryEditorDialogComponent).afterClosed().subscribe((result: Entry) => {
       if (result) {
-        this.entries.unshift(new Entry(result));
+        const entry = new Entry(result);
+        entry.isNew = true;
+
+        this.entries.unshift(entry);
       }
     });
   }
