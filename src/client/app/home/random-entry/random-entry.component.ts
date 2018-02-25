b3abb24d1a83a59ac45e8b6ab535b38796534270
Pavlo Glazkov <p.glazkov@gmail.com>
2018-02-25 13:28:09 +0100

    Add animations to random entry change

--- src/client/app/home/random-entry/random-entry.component.ts
+++ src/client/app/home/random-entry/random-entry.component.ts
@@ -1,10 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Entry } from 'shared';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-random-entry',
   templateUrl: './random-entry.component.html',
-  styleUrls: ['./random-entry.component.scss']
+  styleUrls: ['./random-entry.component.scss'],
+  animations: [
+    trigger('entryChange', [
+      transition('* => *', [
+        animate('0.3s ease-in-out', keyframes([
+          style({ opacity: 0, offset: 0 }),
+          style({ opacity: 0, offset: 0.3 }),
+          style({ opacity: 1, offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class RandomEntryComponent implements OnInit {
   @Input()
