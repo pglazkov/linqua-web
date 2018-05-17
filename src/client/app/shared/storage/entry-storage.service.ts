20b30b197af9ddc9221f97396521dc8d1a26a8c3
Pavlo Glazkov <p.glazkov@gmail.com>
2018-05-17 09:02:40 +0200

    fix: remove timestampsInSnapshots setting - it will be needed after upgrade of firebase

--- src/client/app/shared/storage/entry-storage.service.ts
+++ src/client/app/shared/storage/entry-storage.service.ts
@@ -59,43 +59,9 @@ export class EntryStorageService {
 
   constructor(private fba: FirebaseApp, private authService: AuthService, private http: HttpClient, private zone: NgZone) {
     this.db = fba.firestore();
-    this.configureDb();
     this.persistenceEnabled$ = from(this.db.enablePersistence().then(() => true, () => false));
 
     this.stats$ = this.createStatsStream();
-  }
-
-  private configureDb() {
-    /**
-    The following setting is added because of the following warning from Firebase:
-
-    ==========
-    The behavior for Date objects stored in Firestore is going to change
-    AND YOUR APP MAY BREAK.
-      To hide this warning and ensure your app does not break, you need to add the
-    following code to your app before calling any other Cloud Firestore methods:
-
-      const firestore = firebase.firestore();
-    const settings = {/!* your settings... *!/ timestampsInSnapshots: true};
-    firestore.settings(settings);
-
-    With this change, timestamps stored in Cloud Firestore will be read back as
-      Firebase Timestamp objects instead of as system Date objects. So you will also
-    need to update code expecting a Date to instead expect a Timestamp. For example:
-
-    // Old:
-      const date = snapshot.get('created_at');
-    // New:
-    const timestamp = snapshot.get('created_at');
-    const date = timestamp.toDate();
-
-    Please audit all existing usages of Date when you enable the new behavior. In a
-    future release, the behavior will change to the new behavior, so if you do not
-      follow these steps, YOUR APP MAY BREAK.
-    ==========
-    */
-
-    this.db.settings({ timestampsInSnapshots: true });
   }
 
   getRandomEntryBatch(batchSize: number = 1): Observable<Entry[]> {
