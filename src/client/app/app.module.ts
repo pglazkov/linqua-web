7eb97ce3024cac40ca7205d6e586ca16a733bf8f
Pavlo Glazkov <p.glazkov@gmail.com>
2022-05-12 18:45:39 +0200

    Update service worker setup

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -55,7 +55,12 @@ const materialModules = [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     FormsModule,
     ReactiveFormsModule,
     ...materialModules,
