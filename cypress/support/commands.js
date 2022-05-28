b3a9511c4e5ba763dbe1ea5363dd31856c40e372
Pavlo Glazkov <p.glazkov@gmail.com>
2022-05-28 06:50:01 +0200

    refactor: get rid of the requirement to set `E2E_TEST_ACCOUNT_PASS` variable when using firebase emulators

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,71 +1,45 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, connectAuthEmulator } from 'firebase/auth';
 import { firebaseConfig } from '@linqua/firebase-config';
-
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import { uniqueId } from 'lodash-es';
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const auth = getAuth(firebaseApp);
 
-if (Cypress.env('USE_EMULATOR')) {
+const useEmulator = Cypress.env('USE_EMULATOR');
+
+const testUser = useEmulator ? `${uniqueId('e2e-test')}-${new Date().valueOf()}@linqua-app.com` : 'e2e.ci2@linqua-app.com';
+const testUserPass = useEmulator ? 'e2e-test-pass' : Cypress.env('E2E_TEST_ACCOUNT_PASS');
+
+if (!testUserPass && !useEmulator) {
+  // On the development machine, the E2E_TEST_ACCOUNT_PASS environment variable can be set in command line as `export CYPRESS_E2E_TEST_ACCOUNT_PASS=<password>`
+  // or permanently stored in ~/.bash_profile.
+  throw new Error('E2E_TEST_ACCOUNT_PASS environment variable is not set. Please set it with a password for e2e.ci2@linqua-app.com account.');
+}
+
+if (useEmulator) {
   connectAuthEmulator(auth, 'http://localhost:9099');
 }
 
 Cypress.Commands.add('login', () => {
   cy.logout();
-  if (Cypress.env('CREATE_TEST_ACCOUNT')) {
+  if (useEmulator) {
     cy.createTestAccount();
   }
   cy.loginWithTestAccount();
 });
 
 Cypress.Commands.add('loginWithTestAccount', () => {
-  const pass = Cypress.env('E2E_TEST_ACCOUNT_PASS');
-  if (!pass) {
-    throw new Error('E2E_TEST_ACCOUNT_PASS environment variable is not set. Please set it with a password for e2e.ci2@linqua-app.com account.');
-  }
-
   return new Cypress.Promise((resolve, reject) => {
-    return signInWithEmailAndPassword(auth, 'e2e.ci2@linqua-app.com', Cypress.env('E2E_TEST_ACCOUNT_PASS'))
+    return signInWithEmailAndPassword(auth, testUser, testUserPass)
       .then(resolve, reject);
   });
 });
 
 Cypress.Commands.add('createTestAccount', () => {
-  // On the development machine, the E2E_TEST_ACCOUNT_PASS environment variable can be set in command line as `export CYPRESS_E2E_TEST_ACCOUNT_PASS=<password>`
-  // or permanently stored in ~/.bash_profile.
-  const pass = Cypress.env('E2E_TEST_ACCOUNT_PASS');
-  if (!pass) {
-    throw new Error('E2E_TEST_ACCOUNT_PASS environment variable is not set. Please set it with a password for e2e.ci2@linqua-app.com account.');
-  }
-
   return new Cypress.Promise((resolve, reject) => {
-    return createUserWithEmailAndPassword(auth, 'e2e.ci2@linqua-app.com', Cypress.env('E2E_TEST_ACCOUNT_PASS'))
+    return createUserWithEmailAndPassword(auth, testUser, testUserPass)
       .catch(error => {
         switch (error.code) {
           case 'auth/email-already-in-use':
@@ -102,3 +76,30 @@ function clearIndexedDb(name) {
     request.onsuccess = request.onerror = request.onblocked = () => resolve();
   });
 }
+
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
