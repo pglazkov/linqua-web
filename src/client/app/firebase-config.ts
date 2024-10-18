a21b5c6736a1e8b773ffc83523e9d81e093e4858
Pavlo Glazkov <p.glazkov@gmail.com>
2024-10-18 16:54:05 +0200

    fix: authentication not working anymore with latest browsers

--- src/client/app/firebase-config.ts
+++ src/client/app/firebase-config.ts
@@ -2,7 +2,7 @@ import { FirebaseOptions } from "firebase/app";
 
 export const firebaseConfig: FirebaseOptions = {
   apiKey: 'AIzaSyAzraaEABvAbytZVZRVlguP7XEB1hjY_dE',
-  authDomain: 'linqua-cab88.firebaseapp.com',
+  authDomain: 'linqua-app.com',
   projectId: 'linqua-cab88',
   storageBucket: 'linqua-cab88.appspot.com',
   messagingSenderId: '64353876836'
