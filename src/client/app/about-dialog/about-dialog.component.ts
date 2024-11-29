607ce477bdd5edfaedbf4e840355006e0fb0362a
Pavlo Glazkov <p.glazkov@gmail.com>
2024-11-29 06:13:14 +0100

    refactor: migrate `AboutComponent` to `OnPush`

--- src/client/app/about-dialog/about-dialog.component.ts
+++ src/client/app/about-dialog/about-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialogContent } from '@angular/material/dialog';
 import { environment } from 'environments/environment';
 
@@ -8,6 +8,7 @@ import { environment } from 'environments/environment';
   styleUrls: ['./about-dialog.component.scss'],
   standalone: true,
   imports: [MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutDialogComponent {
   version = environment.version;
