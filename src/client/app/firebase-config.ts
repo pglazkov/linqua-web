fa55b45472010702dafa74216db44ffb706a7370
Pavlo Glazkov <p.glazkov@gmail.com>
2024-12-14 10:37:35 +0100

    chore: switch firebase config to point to an app configured in firebase console

--- src/client/app/firebase-config.ts
+++ src/client/app/firebase-config.ts
@@ -1,9 +1,11 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyAzraaEABvAbytZVZRVlguP7XEB1hjY_dE',
+  apiKey: 'AIzaSyCQCJ3YqdQRsIXDxSo_I0nR0x3UkbokhoM',
   authDomain: 'linqua-app.com',
+  databaseURL: 'https://linqua-cab88.firebaseio.com',
   projectId: 'linqua-cab88',
   storageBucket: 'linqua-cab88.appspot.com',
   messagingSenderId: '64353876836',
+  appId: '1:64353876836:web:462fccf1c20d508425d871',
 };
